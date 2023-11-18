923435f845d410ec65af073f150367fce6358007
tomio13 <haraszti@dwi.rwth-aachen.de>
2023-11-18 17:32:21 +0100

    upped version, units is a major change

--- python/rdm_modules/project_config.py
+++ python/rdm_modules/project_config.py
@@ -41,7 +41,7 @@ def get_default_config() -> dict:
         return value
         a dict containing the configuration parameters.
     """
-    version = 0.36
+    version = 0.40
 
     if os.name == 'posix':
         home_dir = os.getenv('HOME')
